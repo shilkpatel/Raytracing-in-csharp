225e2a703959e171ce307a8e807e5d50f1249a5e
shilkpatel <125868603+shilkpatel@users.noreply.github.com>
2023-04-06 20:07:58 +0100

    normals

--- Rendering_engine/Rendering_engine/ray.cs
+++ Rendering_engine/Rendering_engine/ray.cs
@@ -17,26 +17,44 @@ namespace Rendering_engine
             this.origin = origin;
         }
 
-        bool hit_sphere(vec centre, double radius)
+        public vec at(double t) 
+        {
+            return this.origin + (this.direction * t);
+        }
+
+        double hit_sphere(vec centre, double radius)
         {
             vec oc = origin - centre;
             double a = direction * direction;
             double b = 2 * (oc * direction);
             double c = (oc * oc) - (radius * radius);
             double discriminant = b * b - 4 * a * c;
-            return (discriminant > 0);
+            if (discriminant > 0)
+            {
+                return (-b - discriminant) / 2 * a;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
 
         public double[] ray_colour()
         {
+            double[] colour = new double[3]; 
             vec sphere_origin = new vec(0, 0, 10);
             double radius = 1;
-            if (hit_sphere(sphere_origin, radius)) 
+            double t = hit_sphere(sphere_origin, radius); 
+            if (t > 0) 
             {
-                return new double[] { 255, 0, 0 };
+                vec N = (this.at(t) - sphere_origin).unit();
+                colour[0] = (N.x() + 1) * 0.5 * 255;
+                colour[1] = (N.y() + 1) * 0.5 * 255;
+                colour[2] = (N.z() + 1) * 0.5 * 255;
+                return colour;
             }
-            return new double[] { 0, 0, 0 };
+            return colour;
 
         }
 
--- Rendering_engine/Rendering_engine/vec.cs
+++ Rendering_engine/Rendering_engine/vec.cs
@@ -13,8 +13,11 @@ namespace Rendering_engine
             vector[1] = y;
             vector[2] = z;
         }
+        public vec() { }
 
-
+        public double x() { return vector[0]; }
+        public double y() { return vector[1]; }
+        public double z() { return vector[2]; }
 
 
         public static vec operator +(vec a, vec b)
@@ -39,6 +42,32 @@ namespace Rendering_engine
             double y = a.vector[1] * b.vector[1];
             double z = a.vector[2] * b.vector[2];
             return x+y+z;
+        }
+
+        public static vec operator *(vec a, double b) 
+        {
+            a.vector[0] *= b;
+            a.vector[1] *= b;
+            a.vector[2] *= b;
+
+            return a;
+
+        }
+
+        public double norm() 
+        {
+            return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+        }
+
+        public vec unit() 
+        {
+            vec unit_vector = new vec();
+            double norm = this.norm();
+            unit_vector.vector[0] = this.vector[0] / norm;
+            unit_vector.vector[1] = this.vector[1] / norm;
+            unit_vector.vector[2] = this.vector[2] / norm;
+
+            return unit_vector;
         }
     }
 }
